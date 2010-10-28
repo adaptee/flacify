58463c4a01bae09013df6c0b860d80190e0a5c6b
Adaptee <adaptee@gmail.com>
2010-10-28 20:04:41 +0800

    it works a bit now

---
+++ cuelex.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+import ply.lex as lex
+from ply.lex import TOKEN
+
+# List of token names.   This is always required
+tokens = (
+    'CATALOG', 'CDTEXTFILE', 'FILE', 'FLAGS', 'INDEX', 'ISRC', 'PERFORMER',
+    'POSTGAP', 'PREGAP', 'REM', 'SONGWRITER', 'TITLE', 'TRACK',
+
+    'FILETYPE', 'TRACKTYPE',
+    'INDEXNO', 'OFFSET', 'ISRCID',
+
+    'GENRE', 'COMMENT', 'DATE', 'DISCID',
+    'DATEVALUE',
+
+    'VALUE',
+)
+
+# Regular expression rules for simple tokens
+twodigits = r'\d{2}'
+year      = r'(19|20)\d{2}'
+date      = year + r'(-|/)' + twodigits + r'(-|/)' + twodigits
+
+# A string containing ignored characters (spaces and tabs and linebreaks)
+t_ignore  = ' \t\n'
+
+# Error handling rule
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+# the priority order is as-is
+
+#----cuesheet commands
+def t_CATALOG(t):
+    r'\bCATALOG\b'
+    return t
+
+def t_CDTEXTFILE(t):
+    r'\bCDTEXTFILE\b'
+    return t
+
+def t_FILE(t):
+    r'\bFILE\b'
+    return t
+
+def t_FLAGS(t):
+    r'\bFILE\b'
+    return t
+
+def t_INDEX(t):
+    r'\bINDEX\b'
+    return t
+
+def t_ISRC(t):
+    r'\bISRC\b'
+    return t
+
+def t_PERFORMER(t):
+    r'\bPERFORMER\b'
+    return t
+
+def t_POSTGAP(t):
+    r'\bPOSTGAP\b'
+    return t
+
+def t_PREGAP(t):
+    r'\bPREGAP\b'
+    return t
+
+def t_REM(t):
+    r'\bREM\b'
+    return t
+
+def t_SONGWRITER(t):
+    r'\bSONGWRITER\b'
+    return t
+
+def t_TITLE(t):
+    r'\bTITLE\b'
+    return t
+
+def t_TRACK(t):
+    r'\bTRACK\b'
+    return t
+
+#----valid FILE type
+def t_FILETYPE(t):
+    r'\b(AIFF|BINARY|MOTOROLA|MP3|WAVE)\b'
+    return t
+
+#----valid track type
+def t_TRACKTYPE(t):
+    r'\b(AUDIO)\b'
+    return t
+
+#--- supported metadata
+
+def t_GENRE(t):
+    r'\bGENRE\b'
+    return t
+
+def t_COMMENT(t):
+    r'\bCOMMENT\b'
+    return t
+
+def t_DATE(t):
+    r'\bDATE\b'
+    return t
+
+def t_DISCID(t):
+    r'\bDISCID\b'
+    return t
+
+# ---- other expected value
+def t_OFFSET(t):
+    r'\b\d{2}:\d{2}:\d{2}\b'
+    return t
+
+def t_INDEXNO(t):
+    r'\b\d{2}\b'
+    return t
+
+def t_ISRCID(t):
+    r'\b[a-zA-Z-0-9]{5}\d{7}\b'
+    return t
+
+def t_YEARVALUE(t):
+    r'\b(19|20)\d{2}\b'
+    return t
+
+@TOKEN(date)
+def t_DATEVALUE(t):
+    return t
+
+def t_VALUE(t):
+    r'[^\n]+'
+    t.value = t.value.strip('"')
+    return t
+
+# Build the lexer
+lexer = lex.lex()
+
+if __name__ == '__main__':
+
+
+    # Test it out
+    data = u'''
+    TRACK 02 AUDIO
+    TITLE "days"
+    PERFORMER "CHINO"
+    ISRC 000000000000
+    INDEX 00 04:10:50
+    INDEX 01 04:11:68
+    '''
+    #f  = open("1.cue")
+    #data = f.read()
+
+
+    # Give the lexer some input
+    lexer.input(data)
+
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok: break      # No more input
+        print tok
+
+
---
+++ cueyacc.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""
+
+    catalog    : CATALOG VALUE
+    cdtextfile : CDTEXTFILE VALUE
+    flags      : FLAGS VALUE
+    isrc       : ISRC ISRCID
+    postgap    : POSTGAP VALUE
+    pregap     : PREGAP VALUE
+    performer  : PERFORMER VALUE
+
+
+  simpl-entry   : CATALOG VALUE
+                | CDTEXTFILE VALUE
+                | FLAGS VALUE
+                | ISRC ISCRID
+                | POSTGAP VALUE
+                | PREGAP VALUE
+                | PERFORMER VALUE
+
+  rem-entry     : REM GENRE VALUE
+                | REM COMMENT VALUE
+                | REM DATE DATEVALUE
+                | REM DISCID VALUE
+                | REM KEYWORD VALUE
+
+  nest-entry :
+
+                | INDEX INDEXNO OFFSET
+
+  opt-entries :
+
+  man-entries :
+
+  cue        :
+
+
+"""
+
+import ply.yacc as yacc
+
+# Get the token map from the correspoding lexer.  This is required.
+from cuelex import tokens
+
+#start = 'subentry'
+
+# Error rule for syntax errors
+def p_error(p):
+    print "Syntax error in input!"
+
+#def p_empty(p):
+    #'empty :'
+    #pass
+
+
+#def p_track(p):
+    #r'track : TRACK TRACKNO DATATYPE subentries '
+    #pass
+
+#def p_dummy(p):
+    #r'''
+    #dummy : subentry
+    #'''
+
+    #p[0] = p[1]
+
+def p_dummy(p):
+    r'''
+    dummy :  dummy subentry
+          |  subentry
+    '''
+    #p[0] = "merged"
+
+    if len(p) == 3:
+        p[0] =  p[1] + p[2]
+    else:
+        p[0] = p[1]
+
+def p_subentry(p):
+    r'''
+    subentry : title
+             | performer
+             | isrc
+             | index
+    '''
+
+    p[0] = [ p[1] ]
+
+def p_title(p):
+    r'title : TITLE VALUE'
+    p[0] = ('title', p[2] )
+
+def p_performer(p):
+    r'performer : PERFORMER VALUE'
+    p[0] = ('performer', p[2] )
+
+def p_isrc(p):
+    r'isrc : ISRC ISRCID'
+    p[0] = ('isrc', p[2])
+
+def p_index(p):
+    r'index : INDEX INDEXNO OFFSET'
+    p[0] = ('offset', p[3] )
+
+
+#def p_catalog(p):
+    #r'catalog : CATALOG VALUE'
+
+    #p[0]  = " ".join([ p[1], p[2] ] ) + "\n"
+
+#def p_cdtextfile(p):
+    #r'catalog : CDTEXTFILE VALUE'
+
+    #p[0]  = " ".join([ p[1], p[2] ] ) + "\n"
+
+#def p_flags(p):
+    #r'catalog : FLAGS VALUE'
+
+    #p[0]  = " ".join([ p[1], p[2] ] ) + "\n"
+
+#def p_index(p):
+    #r'index : INDEX INDEXNO OFFSET'
+
+    #p[0]  = " ".join([ p[1], p[2], p[3] ] ) + "\n"
+
+#def p_genre(p):
+    #r'genre : REM GENRE VALUE'
+
+    #p[0]  = " ".join([ p[1], p[2], p[3] ] ) + "\n"
+
+if __name__ == "__main__" :
+
+    # Build the parser
+    parser = yacc.yacc()
+
+    #data = u'''
+    #TITLE "days"
+    #'''
+
+    data = u'''
+    TITLE "days"
+    PERFORMER "CHINO"
+    ISRC 000000000000
+    INDEX 01 04:11:68
+    '''
+
+    result = parser.parse(data)
+    print result
+
