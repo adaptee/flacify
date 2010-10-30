c91fa27ecf1fda2ce6cee02d3fb8c7228ca71ce4
Adaptee <adaptee@gmail.com>
2010-10-30 10:10:15 +0800

    now support more invoking forms

--- 2flacs.py
+++ 2flacs.py
@@ -16,17 +16,42 @@ ext2decoder    = {
                     ".wv"   : "wvunpack",
                  }
 
-decoder_checking             = { }
+decoder_checking = { }
 decoder_checking["mac"]      = "please install mac"
 decoder_checking["flac"]     = "please install flac"
 decoder_checking["wvunpack"] = "please install wavpack"
 decoder_checking["ttaenc"]   = "please install ttaenc"
 
 
+supported_exts = [
+                    '.ape' , '.APE' ,
+                    '.flac', '.FLAC',
+                    '.tta' , '.TTA' ,
+                    '.wv'  , '.WV'  ,
+                    '.wav' , '.WAV' ,
+                 ]
+
+ext_cue_variants = [
+                    'cue',
+                    'CUE',
+                    'Cue',
+                   ]
+
+
+def infomsg (text):
+    print (text)
+
+
 def splitwrapper_both(chunk, cuefile):
-    pass
+
+    infomsg ( ("parsing cuesheet: %s...") % (cuefile) )
+    cuesheet = parsecuefile(cuefile)
+    split(chunk, cuesheet)
+
 
 def splitwrapper_only_chunk(chunk):
+
+    infomsg("only chunk")
 
     basename, ext = os.path.splitext(chunk)
     ext = ext.lower()
@@ -43,17 +68,59 @@ def splitwrapper_only_chunk(chunk):
         print (e)
         return
 
-    print ("analyazing cuesheet...")
     cuefile = pickcuefile(chunk)
-    cuesheet = parsecuefile(cuefile)
 
-    split(chunk, cuesheet)
+    splitwrapper_both(chunk, cuefile)
+
 
 def splitwrapper_only_cuefile(cuefile):
-    pass
+
+    infomsg("only cuefile")
+    infomsg("cuefile:%s" % (cuefile) )
+
+    chunk = pickchunk(cuefile)
+    splitwrapper_both(chunk, cuefile)
+
+
+def pickchunk(cuefile):
+
+    chunk = u""
+
+    basename,  _ = os.path.splitext(cuefile)
+
+    candicates = map( lambda ext: basename + ext , supported_exts)
+    real_candicates = filter ( lambda path : os.path.exists(path), candicates)
+
+    bestchoice = real_candicates[0]
+    #bestchoice = basename + u".ape"
+
+    return bestchoice
 
 def splitwrapper_none():
-    pass
+
+    infomsg("none")
+
+    chunk   = u""
+    cuefile = u""
+
+    for ext in supported_exts:
+        pattern = "*%s" % (ext)
+        matches = glob(pattern)
+
+        if len(matches) > 1 :
+            raise ValueError( " mutiple chunks are found!")
+        elif len(matches) == 1:
+            chunk = matches[0]
+            break
+        else:
+            pass
+
+    if not chunk :
+        raise ValueError( "no chunk is found!")
+
+    cuefile = pickcuefile(chunk)
+    splitwrapper_both(chunk, cuefile)
+
 
 def checkdecoder( decoder, error_msg):
 
@@ -68,11 +135,22 @@ def pickcuefile(chunk):
 
     basename, extenseion = os.path.splitext(chunk)
 
-    candicates = glob( ( "%s*.cue" % basename) )
+    candicates = [ ]
 
-    # FIXME
+
+    for ext in ext_cue_variants:
+        pattern = "%s*%s" % (basename, ext)
+        matches = glob(pattern)
+
+        candicates += matches
+
+    if not candicates :
+        raise ValueError("no suitable cuesheet is available")
+
+    bestchoice = candicates[0]
+
     # navie logic
-    bestchoice =  basename + ".cue"
+    #bestchoice =  basename + ".cue"
 
     # FIXME
     # rude checking
@@ -120,6 +198,11 @@ def analyze_args(arg1, arg2 ):
 
     return real_chunk, real_cuefile
 
+
+def parse_args():
+    pass
+
+
 if __name__ == "__main__" :
 
     argparser = ArgumentParser(
@@ -140,14 +223,19 @@ if __name__ == "__main__" :
     chunk   = args.chunk.decode("utf8") if args.chunk else u""
     cuefile = args.cuefile.decode("utf8") if args.cuefile else u""
 
-    chunk, cuefile = analyze_args ( chunk, cuefile)
+    chunk, cuefile = analyze_args( chunk, cuefile)
 
-    if chunk and cuefile :
-        splitwrapper_both(chunk, cuefile)
-    elif chunk:
-        splitwrapper_only_chunk(chunk)
-    elif cuefile:
-        splitwrapper_only_cuefile(chunk)
-    else:
-        splitwrapper_none()
+    try :
+        if chunk and cuefile :
+            splitwrapper_both(chunk, cuefile)
+        elif chunk:
+            splitwrapper_only_chunk(chunk)
+        elif cuefile:
+            splitwrapper_only_cuefile(cuefile)
+        else:
+            splitwrapper_none()
+    except Exception as e:
+        print (e)
+
+
 
